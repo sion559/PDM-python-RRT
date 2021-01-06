32592d144d2c7043c2f379b83169146f51af7393
Amber Elferink <amber.elferink@gmail.com>
2021-01-06 18:31:38 +0100

    starting point from pillars

---
+++ CoppeliaPillars/Code/flib.py
@@ -0,0 +1,93 @@
+import sim as vrep
+import time
+import math
+import numpy as np
+
+def navigate_local(x, y, z, speed, clientID, QuadricopterT):
+    speed = speed*0.1
+    x_time = math.fabs(x / speed)
+    y_time = math.fabs(y / speed)
+    z_time = math.fabs(z / speed)
+    x_k = 0
+    y_k = 0
+    z_k = 0
+    m = x_timenavigate_local
+    x_m = 0
+    y_m = 0
+    z_m = 0
+    if m < y_time:
+        m = y_time
+    if m < z_time:
+        m = z_time
+    if m != 0:
+        x_m = x_time/m
+        y_m = y_time/m
+        z_m = z_time/m
+    x_sign = 1
+    y_sign = 1
+    z_sign = 1
+    if x!= 0:
+        x_sign = x / math.fabs(x)
+    if y!= 0:
+        y_sign = y/math.fabs(y)
+    if z!=0:
+        z_sign = z / math.fabs(z)
+    while True:
+        if ((x_k < m) and (y_k < m) and (z_k < m)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (speed*x_sign*x_m, speed*y_sign*y_m, speed*z_sign*z_m),
+                                             vrep.simx_opmode_oneshot)
+            time.sleep(0.1)
+            x_k += 1
+            y_k += 1
+            z_k += 1
+        elif ((x_k < m) and (y_k < m) and (z_k == z_time)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (speed*x_sign*x_m, speed*y_sign*y_m, 0.00),
+                                             vrep.simx_opmode_oneshot)
+            time.sleep(0.1)
+            x_k += 1
+            y_k += 1
+        elif ((x_k < x_time) and (y_k == y_time) and (z_k == z_time)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (speed*x_sign, 0.00, 0.00),
+                                             vrep.simx_opmode_oneshot)
+            x_k += 1
+            time.sleep(0.1)
+        elif ((x_k == x_time) and (y_k < y_time) and (z_k == z_time)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (0.00, speed*y_sign, 0.00),
+                                             vrep.simx_opmode_oneshot)
+            y_k += 1
+            time.sleep(0.1)
+        elif ((x_k == x_time) and (y_k == y_time) and (z_k < z_time)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (0.00, 0.00, speed*z_sign),
+                                             vrep.simx_opmode_oneshot)
+            z_k += 1
+            time.sleep(0.1)
+        elif ((x_k == x_time) and (y_k < m) and (z_k < m)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (0.00, speed*y_sign*y_m, speed*z_sign*z_m),
+                                             vrep.simx_opmode_oneshot)
+            y_k += 1
+            z_k += 1
+            time.sleep(0.1)
+        elif ((x_k < m) and (y_k == y_time) and (z_k < m)):
+            err = vrep.simxSetObjectPosition(clientID, QuadricopterT, QuadricopterT, (speed*x_sign*x_m, 0.00, speed*z_sign*z_m),
+                                             vrep.simx_opmode_oneshot)
+            time.sleep(0.1)
+            x_k += 1
+            z_k += 1
+        else:
+            break
+
+def navigate_map(x, y, z, speed, clientID, QuadricopterT):
+    err, pos = vrep.simxGetObjectPosition(
+        clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
+    det_x = x - pos[0]
+    det_y = y - pos[1]
+    det_z = z - pos[2]
+    navigate_local(round(det_x, 1), round(det_y, 1), round(det_z, 1), speed, clientID, QuadricopterT)
+
+def get_pos(clientID, QuadricopterT):
+    err, position = vrep.simxGetObjectPosition(clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
+    return np.array([position[0],position[1],position[2]])
+
+def get_rot(clientID, QuadricopterT):
+    err, orientation = vrep.simxGetObjectOrientation(clientID, QuadricopterT, -1, vrep.simx_opmode_oneshot_wait)
+    return np.array([orientation[0],orientation[1],orientation[2]])
---
+++ CoppeliaPillars/Code/test_2.py
@@ -0,0 +1,118 @@
+import sim
+import math
+import flib
+import time
+from geometry_tools import *
+
+rad = math.radians
+
+targer_point = np.array([0, 0, 1])
+speed = 0.5
+
+r_tolerance = 1.0
+
+c_rep = 1.2
+r_field = 3.
+
+drone_pose = []
+
+def rep_force(dist_to_obs):
+    # type of 1/х
+    return c_rep / dist_to_obs - c_rep / r_field
+
+
+def get_near_obst(current_pose, obst_array):
+    """
+    возвращаем координату до близжайшего препятсвия
+    """
+
+    near_dist = None
+    near_pose = None
+
+    for pose in obst_array:
+        dist = np.linalg.norm([current_pose[0] - pose[0], current_pose[1] - pose[1]])
+        if near_dist is None or dist < near_dist:
+            near_dist = dist
+            near_pose = pose
+            continue
+
+    return near_pose, dist
+
+
+if __name__=="__main__":
+    sim.simxFinish(-1)
+    clientID = sim.simxStart('127.0.0.1', 19997, True, True, 5000, 5)
+
+    if clientID != -1:
+        print('Connected to remote API server')
+    err, QuadricopterT = sim.simxGetObjectHandle(
+        clientID, 'Quadricopter_target', sim.simx_opmode_blocking)
+    if err == -1:
+        print("No Quadricopter")
+    err, Quadricopter = sim.simxGetObjectHandle(
+        clientID, 'Quadricopter', sim.simx_opmode_blocking)
+    if err == -1:
+        print("No Quadricopter")
+
+    sim.simxStartSimulation(clientID, sim.simx_opmode_blocking)
+
+    obst_count = 14
+    obst_list = []
+
+    for i in range(obst_count):
+        err, Obst = sim.simxGetObjectHandle(
+            clientID, 'collum_'+str(i), sim.simx_opmode_blocking)
+        obst_pose = flib.get_pos(clientID, Obst)
+        obst_list.append(obst_pose)
+
+
+    print("is conneted!!!")
+    pose = flib.get_pos(clientID, QuadricopterT)
+    rot = flib.get_rot(clientID, QuadricopterT)
+
+    t = 0
+    old_timer = time.time()
+
+    while True:
+        dt = time.time() - old_timer
+        t += dt
+        old_timer = time.time()
+        if dt < 0.0001:
+            continue
+
+
+        drone_pose = flib.get_pos(clientID, Quadricopter)
+
+
+        # формируем вектор скорости в направлении целевой точки
+        pose = flib.get_pos(clientID, QuadricopterT)
+        goal_vec = targer_point - drone_pose
+        norm_goal_vec = np.linalg.norm(goal_vec)
+        goal_vec = goal_vec / norm_goal_vec
+        # print("goal_vec", goal_vec)
+        goal_vec *= speed
+
+        # формирование вектора отталкивания
+
+        # Получает позици близжайшего препятствия
+        obst_pose, dist_to_obst = get_near_obst(drone_pose, obst_list)
+        print("obst_pose", obst_pose, "dist_to_obst", dist_to_obst)
+
+        # rep_vec = np.array([0,0])
+        rep_vec = obst_pose - drone_pose
+        power = rep_force(dist_to_obst)
+        rep_vec = rep_vec * power
+        print("rep forces", rep_vec)
+
+
+        # публикация в сим
+        err = sim.simxSetObjectPosition(clientID, QuadricopterT, -1,
+                                        (drone_pose[0] + goal_vec[0]-rep_vec[0],
+                                         drone_pose[1] + goal_vec[1]-rep_vec[1],
+                                         drone_pose[2] + +goal_vec[2]),
+                                        sim.simx_opmode_blocking)
+        err = sim.simxSetObjectOrientation(clientID, QuadricopterT, -1, (0, 0, rad(0)), sim.simx_opmode_blocking)
+
+        time.sleep(0.01)
+
+    sim.simxFinish(clientID)
