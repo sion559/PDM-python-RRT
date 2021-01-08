8dda009919eff80364d01e33a6689b6bf1618ba4
Simon <sion559@live.nl>
2021-01-08 14:32:51 +0100

    IT IS SMOOO00TH

--- CoppeliaPillars/Code/test_2.py
+++ CoppeliaPillars/Code/test_2.py
@@ -4,11 +4,11 @@ import flib
 import time
 from geometry_tools import *
 from quad_sim import quadsim_P2P
-import sys, getopt
+import time
+import datetime
 
 rad = math.radians
 
-targer_point = np.array([-9, -10, 5])
 speed = 0.5
 
 r_tolerance = 1.0
@@ -62,61 +62,7 @@ def convert_bbox(pos, size):
 
     return new_size
 
-
-# if __name__=="__main__":
-    
-#     #while True:
-#     #    err = sim.simxSetObjectPosition(clientID, QuadricopterT, -1,
-#     #                                     pos, sim.simx_opmode_blocking)
-#     #    err = sim.simxSetObjectOrientation(clientID, QuadricopterT, -1, ori, sim.simx_opmode_blocking)
-    
-    
-#     # t = 0
-#     # old_timer = time.time()
-#     # while True:
-#     #     dt = time.time() - old_timer
-#     #     t += dt
-#     #     old_timer = time.time()
-#     #     if dt < 0.0001:
-#     #         continue
-
-
-#     #     drone_pose = flib.get_pos(clientID, Quadricopter)
-
-
-#     #     # формируем вектор скорости в направлении целевой точки
-#     #     pose = flib.get_pos(clientID, QuadricopterT)
-#     #     goal_vec = targer_point - drone_pose
-#     #     norm_goal_vec = np.linalg.norm(goal_vec)
-#     #     goal_vec = goal_vec / norm_goal_vec
-#     #     # print("goal_vec", goal_vec)
-#     #     goal_vec *= speed
-
-#     #     # формирование вектора отталкивания
-
-#     #     # Получает позици близжайшего препятствия
-#     #     obst_pose, dist_to_obst = get_near_obst(drone_pose, obst_list)
-#     #     print("obst_pose", obst_pose, "dist_to_obst", dist_to_obst)
-
-#     #     # rep_vec = np.array([0,0])
-#     #     rep_vec = obst_pose - drone_pose
-#     #     power = rep_force(dist_to_obst)
-#     #     rep_vec = rep_vec * power
-#     #     print("rep forces", rep_vec)
-
-
-#     #     # публикация в сим
-#     #     err = sim.simxSetObjectPosition(clientID, QuadricopterT, -1,
-#     #                                     (drone_pose[0] + goal_vec[0]-rep_vec[0],
-#     #                                      drone_pose[1] + goal_vec[1]-rep_vec[1],
-#     #                                      drone_pose[2] + +goal_vec[2]),
-#     #                                     sim.simx_opmode_blocking)
-#     #     err = sim.simxSetObjectOrientation(clientID, QuadricopterT, -1, (0, 0, rad(0)), sim.simx_opmode_blocking)
-
-#     #     time.sleep(0.01)
-
-#     sim.simxFinish(clientID)
-    
+   
 def main():
     obst_count = 17
     targetCount = 2
@@ -140,15 +86,10 @@ def main():
         print("No Quadricopter")
 
     sim.simxStartSimulation(clientID, sim.simx_opmode_oneshot)
+    #sim.simxStartSimulation(clientID, sim.simx_opmode_blocking)
     # enable the synchronous mode on the client:
     sim.simxSynchronous(clientID,1);
-       
-    # enable streaming of a value:
-    anyValue = sim.simxGetIntegerSignal(clientID,"anyValue",sim.simx_opmode_streaming)
-    
-    # enable streaming of the iteration counter:
-    iteration = sim.simxGetIntegerSignal(clientID,"iteration",sim.simx_opmode_streaming)
-
+      
     
     print("is connected!!!")
     
@@ -182,13 +123,15 @@ def main():
     
     print(deliveries)
         
-    pose = flib.get_pos(clientID, QuadricopterT)
+    pose = flib.get_pos(clientID, Quadricopter)
     
     #controller object
     pathControl = quadsim_P2P(pose, bbox_list)
     
     pathControl.iterRun_start()
     
+    now = datetime.datetime.now()
+    lastIter = -1
     for target in deliveries:
         if pathControl.plan(target):
             print("the path is worthy!")
@@ -199,17 +142,18 @@ def main():
         while pathControl.iterRunGo:
             pos, ori = pathControl.iterRun_move()
             #pathControl.display()
-            #print("pos = ",pathControl.iterRun_move())
             
-            err = sim.simxSetObjectPosition(clientID, QuadricopterT, -1,
-                                             pathControl.path[pathControl.pathIter], sim.simx_opmode_blocking)
-            err = sim.simxSetObjectPosition(clientID, Quadricopter, -1,
-                                             pos, sim.simx_opmode_blocking)
-            err = sim.simxSetObjectOrientation(clientID, Quadricopter, -1, ori, sim.simx_opmode_blocking)
+            sim.simxSetObjectPosition(clientID, Quadricopter, -1,
+                                             pos, sim.simx_opmode_streaming)
+            sim.simxSetObjectOrientation(clientID, Quadricopter, -1, ori, sim.simx_opmode_streaming)            
             
-            sim.simxSynchronousTrigger(clientID)
-            #sim.simxGetPingTime(clientID)
-    
+            lastTime = now
+            now = datetime.datetime.now()
+            if(pathControl.pathIter != lastIter):
+                sim.simxSetObjectPosition(clientID, QuadricopterT, -1, pathControl.path[pathControl.pathIter], sim.simx_opmode_streaming)
+                print("Loop time: ", (lastTime-now).microseconds, "[us]")
+                lastIter = pathControl.pathIter
+            
     pathControl.iterRun_stop()
     sim.simxStopSimulation(clientID, sim.simx_opmode_blocking)
     sim.simxFinish(clientID)
