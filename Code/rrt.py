8ca7d11e51176f2f92bad47e8ec49e5dc41150b3
Simon <sion559@live.nl>
2021-01-11 00:08:52 +0100

    clean-up

---
+++ Code/rrt.py
@@ -0,0 +1,320 @@
+"""
+
+Path planning Sample Code with Randomized Rapidly-Exploring Random Trees (RRT)
+
+author: AtsushiSakai(@Atsushi_twi)
+
+"""
+
+import math
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+show_animation = True
+
+
+class RRT:
+    """
+    Class for RRT planning
+    """
+
+    class Node:
+        """
+        RRT Node
+        """
+
+        def __init__(self, x, y, z):
+            self.x = x
+            self.y = y
+            self.z = z
+            self.path_x = []
+            self.path_y = []
+            self.path_z = []
+            self.parent = None
+
+    #editted for 3d
+    def __init__(self,
+                 obstacle_list,
+                 expand_dis=3.0,
+                 path_resolution=0.5,
+                 goal_sample_rate=5,
+                 max_iter=500,
+                 use_funnel=True):
+        """
+        Setting Parameter
+
+        start:Start Position [x,y,z]
+        goal:Goal Position [x,y,z]
+        obstacleList:obstacle Positions [[x,y,z,size],...]
+        max_iter: maximum nuber of iteration in which to find the path
+
+        """
+        
+        self.expand_dis = expand_dis
+        self.path_resolution = path_resolution
+        self.goal_sample_rate = goal_sample_rate
+        self.goal_sample = 0
+        self.max_iter = max_iter
+        self.obstacle_list = obstacle_list
+        self.node_list = []
+        self.imposible = True
+        self.use_funnel = use_funnel
+       
+    #HOMEBREW
+    def prePlan(self, start, goal):
+        """
+        prePlan
+        prepare for search. sets both the goal and start
+        
+        start: 3d-vector with start position
+        goal: 3d-vetor with goal position
+        """
+        self.start = self.Node(start[0], start[1], start[2])
+        self.end = self.Node(goal[0], goal[1],  goal[2])
+        self.goal_node = self.end
+        #setup for conical search
+        self.goalDist, goalTheta, goalPhi = self.calc_distance_and_angle(self.start,self.end)
+        #print("ANGLESSSSSSSSSSS", self.goalDist, "theta ", goalTheta, "phi ", goalPhi)
+        self.goalDist *= 1.1
+        self.goalDir = np.array([(goal[0]-start[0])/self.goalDist, (goal[1]-start[1])/self.goalDist, (goal[2]-start[2])/self.goalDist])
+        ct = math.cos(goalTheta)
+        cp = math.cos(goalPhi)
+        st = math.sin(goalTheta)
+        sp = math.sin(goalPhi)
+        R_z = np.array([[ct,-st,0], #WRONG ROTATION MATRIX, just for magical fixes
+                        [st, ct,0],
+                        [0,  0, 1]])
+        R_y = np.array([[cp,0,sp],
+                        [0, 1,0],
+                        [-sp,0,cp]])
+        self.R = np.matmul(R_y, R_z)
+
+        self.constSinTheta = math.tan(goalTheta/2)
+        
+        for (ox, oy, oz, dx, dy, dz) in self.obstacle_list:
+            ex = goal[0]-ox
+            ey = goal[1]-oy
+            ez = goal[2]-oz                  
+            if ex >= 0 and ex <= dx and ey >= 0 and ey <= dy and ez >= 0 and ez <= dz:
+                print("Goal is in a box")
+                return None
+        
+        self.imposible = False
+        
+        
+    #editted for 3d
+    def planning(self, animation=False):
+        """
+        rrt path planning
+
+        animation: flag for animation on or off
+        """
+        
+        if(self.imposible == True):
+            return None
+        
+        self.node_list = [self.start]
+        for i in range(self.max_iter):
+            rnd_node = None
+            if self.use_funnel:
+                rnd_node = self.get_random_funnel_node()
+            else:
+                rnd_node = self.get_random_node()
+            nearest_ind = self.get_nearest_node_index(self.node_list, rnd_node)
+            nearest_node = self.node_list[nearest_ind]
+
+            new_node = self.steer(nearest_node, rnd_node, self.expand_dis)
+
+            if self.check_collision(new_node, self.obstacle_list):
+                self.node_list.append(new_node)
+
+            if animation and i % 5 == 0:
+                self.draw_graph(rnd_node)
+
+            if self.calc_dist_to_goal(self.node_list[-1].x,
+                                      self.node_list[-1].y,
+                                      self.node_list[-1].z) <= self.expand_dis:
+                final_node = self.steer(self.node_list[-1], self.end,
+                                        self.expand_dis)
+                if self.check_collision(final_node, self.obstacle_list):
+                    return self.generate_final_course(len(self.node_list) - 1)
+
+            if animation and i % 5:
+                self.draw_graph(rnd_node)
+
+        return None  # cannot find path
+
+    #editted for 3d
+    def steer(self, from_node, to_node, extend_length=float("inf")):
+
+        new_node = self.Node(from_node.x, from_node.y, from_node.z)
+        d, theta, phi = self.calc_distance_and_angle(new_node, to_node)
+
+        new_node.path_x = [new_node.x]
+        new_node.path_y = [new_node.y]
+        new_node.path_z = [new_node.z]
+
+        if extend_length > d:
+            extend_length = d
+
+        n_expand = math.floor(extend_length / self.path_resolution)
+
+        for _ in range(n_expand):
+            new_node.x += self.path_resolution * math.cos(theta) * math.cos(phi)
+            new_node.y += self.path_resolution * math.sin(theta) * math.cos(phi)
+            new_node.z += self.path_resolution * math.sin(phi)
+            new_node.path_x.append(new_node.x)
+            new_node.path_y.append(new_node.y)
+            new_node.path_z.append(new_node.z)
+
+        d, _, _ = self.calc_distance_and_angle(new_node, to_node)
+        if d <= self.path_resolution:
+            new_node.path_x.append(to_node.x)
+            new_node.path_y.append(to_node.y)
+            new_node.path_z.append(to_node.z)
+            new_node.x = to_node.x
+            new_node.y = to_node.y
+            new_node.z = to_node.z
+
+        new_node.parent = from_node
+        #print(new_node.x, new_node.y, new_node.z)
+
+        return new_node
+
+    #editted for 3d
+    def generate_final_course(self, goal_ind):
+        path = [[self.end.x, self.end.y, self.end.z]]
+        node = self.node_list[goal_ind]
+        while node.parent is not None:
+            path.append([node.x, node.y, node.z])
+            node = node.parent
+        path.append([node.x, node.y, node.z])
+
+        return path
+
+    #editted for 3d
+    def calc_dist_to_goal(self, x, y, z):
+        dx = x - self.end.x
+        dy = y - self.end.y
+        dz = z - self.end.z
+        return math.hypot(math.hypot(dx,dy), dz)
+
+    #Values are a bit larger than the size of the city in the simulation
+    def get_random_node(self):
+        return self.Node(random.uniform(-15, 15),
+                         random.uniform(-15, 15),
+                         random.uniform(-15, 15))
+    
+    #HOMEBREW
+    def get_random_funnel_node(self, it):
+        """
+        get_random_node
+        generate a random node from a conical field. 
+        
+        Returns random node or 'end' once every goal_sample_rate calls
+        """
+        
+        if self.goal_sample == 0:
+            self.goal_sample = self.goal_sample_rate
+            return self.end
+        else:
+            self.goal_sample -= 1
+
+        dist = self.goalDist
+        if(it < 200): 
+            dist = (self.goalDist*it)/200 + self.connect_circle_dist
+
+        #TODO generate in a cone
+        L = random.uniform(0, dist)
+        if(L < self.connect_circle_dist):
+            a = self.connect_circle_dist
+            return self.Node(random.uniform(self.start.x-a, self.start.x+a),
+                             random.uniform(self.start.y-a, self.start.y+a),
+                             random.uniform(self.start.z-a, self.start.z+a))
+        
+        r = random.uniform(0, L*self.constSinTheta)
+        p = random.uniform(0, 6.28)     #angle aroung goalDir
+        #cone_center = self.goalDir*L
+        rNode = np.matmul(self.R, np.array([L, r*math.cos(p), r*math.sin(p)]))
+
+        #print(rNode)
+        rnd = self.Node(rNode[0] + self.start.x, rNode[1] + self.start.y, rNode[2] + self.start.z)      
+        
+        #print(rNode)
+        return rnd
+    
+    #ORIGINAL
+    def draw_graph(self, rnd=None):
+        plt.clf()
+        # for stopping simulation with the esc key.
+        plt.gcf().canvas.mpl_connect(
+            'key_release_event',
+            lambda event: [exit(0) if event.key == 'escape' else None])
+        if rnd is not None:
+            plt.plot(rnd.x, rnd.y, "^k")
+        for node in self.node_list:
+            if node.parent:
+                plt.plot(node.path_x, node.path_y, "-g")
+
+        for (ox, oy, size) in self.obstacle_list:
+            self.plot_circle(ox, oy, size)
+
+        plt.plot(self.start.x, self.start.y, "xr")
+        plt.plot(self.end.x, self.end.y, "xr")
+        plt.axis("equal")
+        plt.axis([-2, 15, -2, 15])
+        plt.grid(True)
+        plt.pause(0.01)
+
+    #ORIGINAL
+    @staticmethod
+    def plot_circle(x, y, size, color="-b"):  # pragma: no cover
+        deg = list(range(0, 360, 5))
+        deg.append(0)
+        xl = [x + size * math.cos(np.deg2rad(d)) for d in deg]
+        yl = [y + size * math.sin(np.deg2rad(d)) for d in deg]
+        plt.plot(xl, yl, color)
+
+    #editted for 3d
+    @staticmethod
+    def get_nearest_node_index(node_list, rnd_node):
+        dlist = [(node.x - rnd_node.x)**2 + (node.y - rnd_node.y)**2 + (node.z - rnd_node.z)**2
+                 for node in node_list]
+ 
+        return dlist.index(min(dlist))
+
+    #edit for 3d and rectangular obstacles
+    @staticmethod
+    def check_collision(node, obstacleList):
+        
+        if node is None:
+            print("Geen nits")
+            return False
+        
+        if node.z <= 0:
+            return False
+
+        #calculate the distance between obstacle edges and node
+        for (ox, oy, oz, dx, dy, dz) in obstacleList:
+            dx_list = [x - ox for x in node.path_x]
+            dy_list = [y - oy for y in node.path_y]
+            dz_list = [z - oz for z in node.path_z]
+            
+            for ex,ey,ez in zip(dx_list, dy_list, dz_list):
+                if ex >= 0 and ex <= dx and ey >= 0 and ey <= dy and ez >= 0 and ez <= dz:
+                    return False
+                
+        return True  # safe
+
+    #editted for 3d
+    @staticmethod
+    def calc_distance_and_angle(from_node, to_node):
+        dx = to_node.x - from_node.x
+        dy = to_node.y - from_node.y
+        dz = to_node.z - from_node.z
+        d = math.sqrt(dx**2 + dy**2 + dz**2)
+        theta = math.atan2(dy, dx)
+        phi = math.atan2(dz, math.hypot(dx, dy))
+        return d, theta, phi
---
+++ Code/rrt_star.py
@@ -0,0 +1,276 @@
+"""
+
+Path planning Sample Code with RRT*
+
+author: Atsushi Sakai(@Atsushi_twi)
+
+"""
+
+import math
+import sim
+import numpy as np
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../RRT/")
+
+try:
+    from rrt import RRT
+except ImportError:
+    raise
+
+show_animation = True
+
+
+class RRTStar(RRT):
+    """
+    Class for RRT Star planning
+    """
+
+    class Node(RRT.Node):
+        def __init__(self, x, y, z):
+            super().__init__(x, y, z)
+            self.cost = 0.0
+
+    def __init__(self,
+                 obstacle_list,
+                 searchTheta=3.14/4,
+                 expand_dis=1.5,
+                 path_resolution=0.05,
+                 goal_sample_rate=4,
+                 max_iter=10000,
+                 connect_circle_dist=4,
+                 search_until_max_iter=False,
+                 use_funnel = True):
+        """
+        Setting Parameter
+
+        start:Start Position [x,y,z]
+        goal:Goal Position [x,y,z]
+        obstacleList:obstacle Positions [[pos_x, pos_y, pos_z, len_x, len_y, len_z],...]
+        searchTheta: angles for conical search area
+        max_iter: maximum nuber of iteration in which to find the path
+        connect_circle_dist: circle radius in which optimize node connections
+
+        """
+        super().__init__(obstacle_list, expand_dis,
+                         path_resolution, goal_sample_rate, max_iter, use_funnel)
+        self.connect_circle_dist = connect_circle_dist
+        self.search_until_max_iter = search_until_max_iter
+        self.searchTheta = searchTheta
+
+    #edit for 3d
+    def planning(self, clientID):
+        """
+        planning
+        plan a path between the start and goal nodes.
+        prePlan(start, goal) needs to be called first of this function will fail
+        
+        Returns 'None' if no path can be found
+        Returns path when a valid path is found
+        
+        """
+        err, dodo = sim.simxGetObjectHandle(
+            clientID, 'dodo', sim.simx_opmode_blocking)
+
+        
+        if(self.imposible == True):
+            return None        
+        
+        self.node_list = [self.start]
+        for i in range(self.max_iter):
+            
+            rnd = None
+            #generate random node
+            if self.use_funnel:
+                rnd = self.get_random_funnel_node(i)
+            else:
+                rnd = self.get_random_node()
+            #sim.simxSetObjectPosition(clientID, dodo, -1, [rnd.x, rnd.y, rnd.z] 
+            #    , sim.simx_opmode_streaming)
+            # sim.simxSetObjectPosition(clientID, dodo, -1, [self.start.x, self.start.y, self.start.z] 
+            #     + np.dot(self.R.T, np.array([1, 0, 0]) * self.calc_dist_to_goal(rnd.x, rnd.y, rnd.z)), sim.simx_opmode_streaming)
+
+            #find nearest node
+            nearest_ind = self.get_nearest_node_index(self.node_list, rnd)
+            
+            #connect nearest node to the new node
+            new_node = self.steer(self.node_list[nearest_ind], rnd, self.expand_dis)
+            near_node = self.node_list[nearest_ind]
+            
+            #calculate cost for nearest node
+            new_node.cost = self.calc_new_cost(near_node, new_node)
+
+            #check collision and connect new node to tree structure
+            if self.check_collision(new_node, self.obstacle_list):
+                near_inds = self.find_near_nodes(new_node)
+                node_with_updated_parent = self.choose_parent(new_node, near_inds)
+                if node_with_updated_parent:
+                    self.rewire(node_with_updated_parent, near_inds)
+                    self.node_list.append(node_with_updated_parent)
+                else:
+                    self.node_list.append(new_node)
+                sim.simxSetObjectPosition(clientID, dodo, -1, [new_node.x, new_node.y, new_node.z] 
+                , sim.simx_opmode_streaming)
+               
+            #check for the goal
+            if ((not self.search_until_max_iter) and new_node):  # if reaches goal
+                last_index = self.search_best_goal_node()
+                if last_index is not None:
+                    tmp = self.generate_final_course(last_index)
+                    tmp.reverse()
+                    self.nr_iterations = i
+                    self.nodes = len(self.node_list)
+                    print("Iter:", i, ", number of nodes:", len(self.node_list))
+                    return tmp
+
+        print("Max iteration, number of nodes:", len(self.node_list))
+
+        last_index = self.search_best_goal_node()
+        if last_index is not None:
+            return self.generate_final_course(last_index)
+
+        return None
+
+    #ORIGINAL
+    def choose_parent(self, new_node, near_inds):
+        """
+        Computes the cheapest point to new_node contained in the list
+        near_inds and set such a node as the parent of new_node.
+            Arguments:
+            --------
+                new_node, Node
+                    randomly generated node with a path from its neared point
+                    There are not coalitions between this node and th tree.
+                near_inds: list
+                    Indices of indices of the nodes what are near to new_node
+
+            Returns.
+            ------
+                Node, a copy of new_node
+        """
+        if not near_inds:
+            return None
+
+        # search nearest cost in near_inds
+        costs = []
+        for i in near_inds:
+            near_node = self.node_list[i]
+            t_node = self.steer(near_node, new_node)
+            if t_node and self.check_collision(t_node, self.obstacle_list):
+                costs.append(self.calc_new_cost(near_node, new_node))
+            else:
+                costs.append(float("inf"))  # the cost of collision node
+        min_cost = min(costs)
+
+        if min_cost == float("inf"):
+            print("There is no good path.(min_cost is inf)")
+            return None
+
+        min_ind = near_inds[costs.index(min_cost)]
+        new_node = self.steer(self.node_list[min_ind], new_node)
+        new_node.cost = min_cost
+
+        return new_node
+
+    #edit for 3d
+    def search_best_goal_node(self):
+        dist_to_goal_list = [
+            self.calc_dist_to_goal(n.x, n.y, n.z) for n in self.node_list
+        ]
+        goal_inds = [
+            dist_to_goal_list.index(i) for i in dist_to_goal_list
+            if i <= self.expand_dis
+        ]
+
+        safe_goal_inds = []
+        for goal_ind in goal_inds:
+            t_node = self.steer(self.node_list[goal_ind], self.goal_node)
+            if self.check_collision(t_node, self.obstacle_list):
+                safe_goal_inds.append(goal_ind)
+
+        if not safe_goal_inds:
+            return None
+
+        min_cost = min([self.node_list[i].cost for i in safe_goal_inds])
+        for i in safe_goal_inds:
+            if self.node_list[i].cost == min_cost:
+                return i
+
+        return None
+
+    #edit for 3d
+    def find_near_nodes(self, new_node):
+        """
+        1) defines a ball centered on new_node
+        2) Returns all nodes of the tree that are inside this ball
+            Arguments:
+            ---------
+                new_node: Node
+                    new randomly generated node, without collisions between
+                    its nearest node
+            Returns:
+            -------
+                list
+                    List with the indices of the nodes inside the ball of
+                    radius r
+        """
+        nnode = len(self.node_list) + 1
+        r = self.connect_circle_dist * math.sqrt((math.log(nnode) / nnode))
+        # if expand_dist exists, search vertices in a range no more than
+        # expand_dist
+        if hasattr(self, 'expand_dis'):
+            r = min(r, self.expand_dis)
+        dist_list = [(node.x - new_node.x)**2 + (node.y - new_node.y)**2 + (node.z - new_node.z)**2
+                     for node in self.node_list]
+        near_inds = [dist_list.index(i) for i in dist_list if i <= r**2]
+        return near_inds
+
+    #edit for 3d
+    def rewire(self, new_node, near_inds):
+        """
+            For each node in near_inds, this will check if it is cheaper to
+            arrive to them from new_node.
+            In such a case, this will re-assign the parent of the nodes in
+            near_inds to new_node.
+            Parameters:
+            ----------
+                new_node, Node
+                    Node randomly added which can be joined to the tree
+
+                near_inds, list of uints
+                    A list of indices of the self.new_node which contains
+                    nodes within a circle of a given radius.
+            Remark: parent is designated in choose_parent.
+
+        """
+        for i in near_inds:
+            near_node = self.node_list[i]
+            edge_node = self.steer(new_node, near_node)
+            if not edge_node:
+                continue
+            edge_node.cost = self.calc_new_cost(new_node, near_node)
+
+            no_collision = self.check_collision(edge_node, self.obstacle_list)
+            improved_cost = near_node.cost > edge_node.cost
+
+            if no_collision and improved_cost:
+                near_node.x = edge_node.x
+                near_node.y = edge_node.y
+                near_node.z = edge_node.z
+                near_node.cost = edge_node.cost
+                near_node.path_x = edge_node.path_x
+                near_node.path_y = edge_node.path_y
+                near_node.path_z = edge_node.path_z
+                near_node.parent = edge_node.parent
+                self.propagate_cost_to_leaves(new_node)
+
+    #edit for 3d
+    def calc_new_cost(self, from_node, to_node):
+        d, _, _ = self.calc_distance_and_angle(from_node, to_node)
+        return from_node.cost + d
+
+    #ORIGINAL
+    def propagate_cost_to_leaves(self, parent_node):
+
+        for node in self.node_list:
+            if node.parent == parent_node:
+                node.cost = self.calc_new_cost(parent_node, node)
+                self.propagate_cost_to_leaves(node)
